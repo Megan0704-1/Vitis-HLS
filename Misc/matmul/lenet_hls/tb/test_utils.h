d208aec47e29c0f7e373768573ea933ac51405ae
megankuo <megankuo@asu.edu>
2025-04-01 13:03:33 -0700

    helper script for tb

--- Misc/matmul/lenet_hls/tb/test_utils.h
+++ Misc/matmul/lenet_hls/tb/test_utils.h
@@ -6,7 +6,18 @@
 
 typedef ap_fixed<8, 3> data_t;
 
-#define IN_DIM 28
+#define CONV1_WEIGHT_BIN "conv1_weights.bin"
+#define CONV1_BIAS_BIN "conv1_bias.bin"
+#define CONV2_WEIGHT_BIN "conv2_weights.bin"
+#define CONV2_BIAS_BIN "conv2_bias.bin"
+#define FC1_WEIGHT_BIN "fc1_weights.bin"
+#define FC1_BIAS_BIN "fc1_bias.bin"
+#define FC2_WEIGHT_BIN "fc2_weights.bin"
+#define FC2_BIAS_BIN "fc2_bias.bin"
+#define FC3_WEIGHT_BIN "fc3_weights.bin"
+#define FC3_BIAS_BIN "fc3_bias.bin"
+#define DATASET "test_img.bin"
+
 
 template<int C, int KERNEL_SIZE>
 void load_conv2d_weights(
@@ -26,6 +37,22 @@ void load_conv2d_weights(
     }
 }
 
+template<int OUT_DIM, int IN_DIM>
+void load_fc_weigts(
+        const char* filename,
+        data_t fc_weights[OUT_DIM][IN_DIM]
+        ) {
+    std::ifstream file(filename, std::ios::binary);
+
+    for(int o=0; o<OUT_DIM; ++o) {
+        for(int i=0; i<IN_DIM; ++i) {
+            float val;
+            file.read(reinterpret_cast<char*>(&val), sizeof(float));
+            fc_weights[o][i] = data_t(val);
+        }
+    }
+}
+
 template<int SIZE>
 void load_bias(
         const char* filename,
@@ -40,6 +67,7 @@ void load_bias(
     }
 }
 
+template<int IN_DIM>
 void load_input_to_stream(
         const char* filename,
         hls::stream<data_t>& in_stream
